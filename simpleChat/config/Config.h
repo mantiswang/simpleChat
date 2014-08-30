8bb2102769791e66e8ab0da5353be2923e9fbdd1
apple <apple@appledeiMac-2.local>
2014-08-30 17:54:50 +0800

    sharesdk

---
+++ simpleChat/config/Config.h
@@ -0,0 +1,14 @@
+//
+//  Config.h
+//  simpleChat
+//
+//  Created by apple on 14-8-30.
+//  Copyright (c) 2014年 wangyue. All rights reserved.
+//
+
+#ifndef simpleChat_Config_h
+#define simpleChat_Config_h
+
+#define UmengAppkey                (@"5401592ffd98c50365005723")
+
+#endif
---
+++ simpleChat/config/Header.h
@@ -0,0 +1,15 @@
+//
+//  Header.h
+//  simpleChat
+//
+//  Created by apple on 14-8-30.
+//  Copyright (c) 2014年 wangyue. All rights reserved.
+//
+
+#ifndef simpleChat_Header_h
+#define simpleChat_Header_h
+
+#import "UMSocial.h"
+#import "EaseMob.h"
+
+#endif
